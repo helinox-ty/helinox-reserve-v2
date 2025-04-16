58a9098086de91e9e54028aa94ced2514e6bd0c9
helinox-ty <tykim@helinox.com>
2025-04-16 14:45:46 +0900

    스타일 리팩토링 & Tailwind 적용 / 예약 기능 코드 정리

--- netlify/functions/utils/supabase.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(
-  'https://nvnbrlgfsqwnjpxaunuh.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bmJybGdmc3F3bmpweGF1bnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1OTA1MjQsImV4cCI6MjA2MDE2NjUyNH0.nP4YSKvolsBau-iMxQvmAqxzYR83luAhFqtHekodmac'
-);
-
-module.exports = supabase; 
